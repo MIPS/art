fc49239707c55df82f7bcee4783710bf5ea1c785
Douglas Leung <douglas.leung@imgtec.com>
2015-02-11 20:35:45 -0800

    Add JNI trampoline for mips32.
    This patch allows the browser to come up.
    Modified from AOSP SHA1: 735b8559d6dd26c610d4b97d72f25a092d9c8947
    
    Change-Id: I5cc9ed96fb77ccae9ca4288e6989d798e06480a0

--- runtime/arch/mips/asm_support_mips.h
+++ runtime/arch/mips/asm_support_mips.h
@@ -26,8 +26,8 @@
 // Offset of field Thread::tlsPtr_.exception verified in InitCpu
 #define THREAD_EXCEPTION_OFFSET 124
 
-#define FRAME_SIZE_SAVE_ALL_CALLEE_SAVE 64
-#define FRAME_SIZE_REFS_ONLY_CALLEE_SAVE 64
+#define FRAME_SIZE_SAVE_ALL_CALLEE_SAVE 48
+#define FRAME_SIZE_REFS_ONLY_CALLEE_SAVE 48
 #define FRAME_SIZE_REFS_AND_ARGS_CALLEE_SAVE 64
 
 // Expected size of a heap reference
