d10db483e5957c55818311efde239b7eab39513a
Alexey Frunze <Alexey.Frunze@imgtec.com>
2017-06-01 21:07:52 -0700

    MIPS: Shorten .bss string/class loads
    This is a follow-up to
    https://android-review.googlesource.com/#/c/384033/.
    
    Test: booted MIPS64 (with 2nd arch MIPS32R6) in QEMU
    Test: testrunner.py --target --optimizing
    Test: same tests as above on CI20
    Test: booted MIPS32R2 and MIPS64 in QEMU in configurations:
          ART_USE_READ_BARRIER=false,
          ART_READ_BARRIER_TYPE=TABLELOOKUP
    
    (backported from commit 5fa5c04ca39fb9c46bfef0e0807a18d0cd9a4ba7)
    
    Change-Id: I4cb2f4ded13c0d9fc960c7eac55396f7931c1e38

--- compiler/linker/mips/relative_patcher_mips32r6_test.cc
+++ compiler/linker/mips/relative_patcher_mips32r6_test.cc
@@ -26,7 +26,9 @@ class Mips32r6RelativePatcherTest : public RelativePatcherTest {
 
  protected:
   static const uint8_t kUnpatchedPcRelativeRawCode[];
-  static const uint32_t kLiteralOffset;
+  static const uint32_t kLiteralOffsetHigh;
+  static const uint32_t kLiteralOffsetLow1;
+  static const uint32_t kLiteralOffsetLow2;
   static const uint32_t kAnchorOffset;
   static const ArrayRef<const uint8_t> kUnpatchedPcRelativeCode;
 
@@ -44,9 +46,11 @@ class Mips32r6RelativePatcherTest : public RelativePatcherTest {
 const uint8_t Mips32r6RelativePatcherTest::kUnpatchedPcRelativeRawCode[] = {
     0x34, 0x12, 0x5E, 0xEE,  // auipc s2, high(diff); placeholder = 0x1234
     0x78, 0x56, 0x52, 0x26,  // addiu s2, s2, low(diff); placeholder = 0x5678
+    0x78, 0x56, 0x52, 0x8E,  // lw    s2, (low(diff))(s2) ; placeholder = 0x5678
 };
-const uint32_t Mips32r6RelativePatcherTest::kLiteralOffset = 0;  // At auipc (where
-                                                                 // patching starts).
+const uint32_t Mips32r6RelativePatcherTest::kLiteralOffsetHigh = 0;  // At auipc.
+const uint32_t Mips32r6RelativePatcherTest::kLiteralOffsetLow1 = 4;  // At addiu.
+const uint32_t Mips32r6RelativePatcherTest::kLiteralOffsetLow2 = 8;  // At lw.
 const uint32_t Mips32r6RelativePatcherTest::kAnchorOffset = 0;  // At auipc (where PC+0 points).
 const ArrayRef<const uint8_t> Mips32r6RelativePatcherTest::kUnpatchedPcRelativeCode(
     kUnpatchedPcRelativeRawCode);
@@ -60,11 +64,12 @@ void Mips32r6RelativePatcherTest::CheckPcRelativePatch(const ArrayRef<const Link
   ASSERT_TRUE(result.first);
 
   uint32_t diff = target_offset - (result.second + kAnchorOffset);
-  diff += (diff & 0x8000) << 1;  // Account for sign extension in addiu.
+  diff += (diff & 0x8000) << 1;  // Account for sign extension in addiu/lw.
 
   const uint8_t expected_code[] = {
       static_cast<uint8_t>(diff >> 16), static_cast<uint8_t>(diff >> 24), 0x5E, 0xEE,
       static_cast<uint8_t>(diff), static_cast<uint8_t>(diff >> 8), 0x52, 0x26,
+      static_cast<uint8_t>(diff), static_cast<uint8_t>(diff >> 8), 0x52, 0x8E,
   };
   EXPECT_TRUE(CheckLinkedMethod(MethodRef(1u), ArrayRef<const uint8_t>(expected_code)));
 }
@@ -73,7 +78,9 @@ void Mips32r6RelativePatcherTest::TestDexCacheReference(uint32_t dex_cache_array
                                                         uint32_t element_offset) {
   dex_cache_arrays_begin_ = dex_cache_arrays_begin;
   LinkerPatch patches[] = {
-      LinkerPatch::DexCacheArrayPatch(kLiteralOffset, nullptr, kAnchorOffset, element_offset)
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetHigh, nullptr, kAnchorOffset, element_offset),
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetLow1, nullptr, kAnchorOffset, element_offset),
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetLow2, nullptr, kAnchorOffset, element_offset)
   };
   CheckPcRelativePatch(ArrayRef<const LinkerPatch>(patches),
                        dex_cache_arrays_begin_ + element_offset);
@@ -83,7 +90,9 @@ void Mips32r6RelativePatcherTest::TestStringReference(uint32_t string_offset) {
   constexpr uint32_t kStringIndex = 1u;
   string_index_to_offset_map_.Put(kStringIndex, string_offset);
   LinkerPatch patches[] = {
-      LinkerPatch::RelativeStringPatch(kLiteralOffset, nullptr, kAnchorOffset, kStringIndex)
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetHigh, nullptr, kAnchorOffset, kStringIndex),
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetLow1, nullptr, kAnchorOffset, kStringIndex),
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetLow2, nullptr, kAnchorOffset, kStringIndex)
   };
   CheckPcRelativePatch(ArrayRef<const LinkerPatch>(patches), string_offset);
 }
--- compiler/linker/mips/relative_patcher_mips_test.cc
+++ compiler/linker/mips/relative_patcher_mips_test.cc
@@ -30,7 +30,9 @@ class MipsRelativePatcherTest : public RelativePatcherTest {
 
  protected:
   static const uint8_t kUnpatchedPcRelativeRawCode[];
-  static const uint32_t kLiteralOffset;
+  static const uint32_t kLiteralOffsetHigh;
+  static const uint32_t kLiteralOffsetLow1;
+  static const uint32_t kLiteralOffsetLow2;
   static const uint32_t kAnchorOffset;
   static const ArrayRef<const uint8_t> kUnpatchedPcRelativeCode;
 
@@ -50,8 +52,11 @@ const uint8_t MipsRelativePatcherTest::kUnpatchedPcRelativeRawCode[] = {
     0x34, 0x12, 0x12, 0x3C,  // lui   s2, high(diff); placeholder = 0x1234
     0x21, 0x90, 0x5F, 0x02,  // addu  s2, s2, ra
     0x78, 0x56, 0x52, 0x26,  // addiu s2, s2, low(diff); placeholder = 0x5678
+    0x78, 0x56, 0x52, 0x8E,  // lw    s2, (low(diff))(s2) ; placeholder = 0x5678
 };
-const uint32_t MipsRelativePatcherTest::kLiteralOffset = 4;  // At lui (where patching starts).
+const uint32_t MipsRelativePatcherTest::kLiteralOffsetHigh = 4;  // At lui.
+const uint32_t MipsRelativePatcherTest::kLiteralOffsetLow1 = 12;  // At addiu.
+const uint32_t MipsRelativePatcherTest::kLiteralOffsetLow2 = 16;  // At lw.
 const uint32_t MipsRelativePatcherTest::kAnchorOffset = 8;  // At addu (where PC+0 points).
 const ArrayRef<const uint8_t> MipsRelativePatcherTest::kUnpatchedPcRelativeCode(
     kUnpatchedPcRelativeRawCode);
@@ -68,13 +73,14 @@ void MipsRelativePatcherTest::CheckPcRelativePatch(const ArrayRef<const LinkerPa
   if (patches[0].GetType() == LinkerPatch::Type::kDexCacheArray) {
     diff += kDexCacheArrayLwOffset;
   }
-  diff += (diff & 0x8000) << 1;  // Account for sign extension in addiu.
+  diff += (diff & 0x8000) << 1;  // Account for sign extension in addiu/lw.
 
   const uint8_t expected_code[] = {
       0x00, 0x00, 0x10, 0x04,
       static_cast<uint8_t>(diff >> 16), static_cast<uint8_t>(diff >> 24), 0x12, 0x3C,
       0x21, 0x90, 0x5F, 0x02,
       static_cast<uint8_t>(diff), static_cast<uint8_t>(diff >> 8), 0x52, 0x26,
+      static_cast<uint8_t>(diff), static_cast<uint8_t>(diff >> 8), 0x52, 0x8E,
   };
   EXPECT_TRUE(CheckLinkedMethod(MethodRef(1u), ArrayRef<const uint8_t>(expected_code)));
 }
@@ -83,7 +89,9 @@ void MipsRelativePatcherTest::TestDexCacheReference(uint32_t dex_cache_arrays_be
                                                     uint32_t element_offset) {
   dex_cache_arrays_begin_ = dex_cache_arrays_begin;
   LinkerPatch patches[] = {
-      LinkerPatch::DexCacheArrayPatch(kLiteralOffset, nullptr, kAnchorOffset, element_offset)
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetHigh, nullptr, kAnchorOffset, element_offset),
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetLow1, nullptr, kAnchorOffset, element_offset),
+      LinkerPatch::DexCacheArrayPatch(kLiteralOffsetLow2, nullptr, kAnchorOffset, element_offset)
   };
   CheckPcRelativePatch(ArrayRef<const LinkerPatch>(patches),
                        dex_cache_arrays_begin_ + element_offset);
@@ -93,7 +101,9 @@ void MipsRelativePatcherTest::TestStringReference(uint32_t string_offset) {
   constexpr uint32_t kStringIndex = 1u;
   string_index_to_offset_map_.Put(kStringIndex, string_offset);
   LinkerPatch patches[] = {
-      LinkerPatch::RelativeStringPatch(kLiteralOffset, nullptr, kAnchorOffset, kStringIndex)
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetHigh, nullptr, kAnchorOffset, kStringIndex),
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetLow1, nullptr, kAnchorOffset, kStringIndex),
+      LinkerPatch::RelativeStringPatch(kLiteralOffsetLow2, nullptr, kAnchorOffset, kStringIndex)
   };
   CheckPcRelativePatch(ArrayRef<const LinkerPatch>(patches), string_offset);
 }
